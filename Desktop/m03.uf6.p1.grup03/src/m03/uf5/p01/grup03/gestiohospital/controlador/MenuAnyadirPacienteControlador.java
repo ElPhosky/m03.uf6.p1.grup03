4cc7117eef26990387b490ca8579902c3434c720
khugar <ksangon98@gmail.com>
2019-03-31 19:21:33 +0200

    Adaptado añadir Paciente a sql 1

--- Desktop/m03.uf6.p1.grup03/src/m03/uf5/p01/grup03/gestiohospital/controlador/MenuAnyadirPacienteControlador.java
+++ Desktop/m03.uf6.p1.grup03/src/m03/uf5/p01/grup03/gestiohospital/controlador/MenuAnyadirPacienteControlador.java
@@ -1,6 +1,11 @@
 package m03.uf5.p01.grup03.gestiohospital.controlador;
 
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 //import static m03.uf5.p01.grup03.gestiohospital.controlador.MenuControlador.control;
@@ -41,67 +46,82 @@ public class MenuAnyadirPacienteControlador extends Controlador {
             Pattern pat = Pattern.compile("^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑçÇäÄëËïÏöÖüÜ\\s]*$");
             Matcher mat = pat.matcher(nombre);
             if (!mat.find()) { //COMPRUEBA NOMBRE
-                salida = (salida+"Revisa Nombre ");
+                salida = (salida + "Revisa Nombre ");
                 temp = false;
             }
             mat = pat.matcher(apellido1);
             if (!mat.find()) { //COMPRUEBA APELLIDO1
 
-                salida = (salida+"Revisa Primer Apellido");
+                salida = (salida + "Revisa Primer Apellido");
                 temp = false;
             }
             mat = pat.matcher(apellido2); //COMPRUEBA APELLIDO 2
             if (!mat.find()) {
-                salida = (salida+"Revisa Segundo Apellido");
+                salida = (salida + "Revisa Segundo Apellido");
                 temp = false;
             }
             pat = Pattern.compile("\\d{8}[A-HJ-NP-TV-Z]");
             mat = pat.matcher(DNI);
             if (!mat.find()) { //COMPRUEBA DNI
-                salida = (salida+"Revisa DNI");
+                salida = (salida + "Revisa DNI");
                 temp = false;
             }
             pat = Pattern.compile("(66|53|50|[0-4][0-9])-?\\d{8}-?\\d{2}");
             mat = pat.matcher(NSS);
             if (!mat.find()) { //COMPRUEBA NSS
-                salida = (salida+"Revisa NSS");
+                salida = (salida + "Revisa NSS");
                 temp = false;
             }
             pat = Pattern.compile("^(?:(?:\\+|00)?34)?[6789]\\d{8}$");
             mat = pat.matcher(Telefon);
             if (!mat.find()) { //COMPRUEBA TELEFON
-                salida = (salida+"Revisa Telefon");
+                salida = (salida + "Revisa Telefon");
                 temp = false;
             }
             try {
                 Integer.parseInt(numero);
             } catch (NumberFormatException e) {
-                salida = (salida+"Revisa el numero");
+                salida = (salida + "Revisa el numero");
                 temp = false;
             }
             try {
                 Integer.parseInt(porta);
             } catch (NumberFormatException e) {
-                salida = (salida+"Revisa la puerta.");
+                salida = (salida + "Revisa la puerta.");
                 temp = false;
             }
 
             if (temp) {
-                Adreca adreca = new Adreca(ciutat, cp, carrer, Integer.parseInt(numero), planta, Integer.parseInt(porta));
-                Pacient pacient = new Pacient(nombre, apellido1, apellido2, DNI, NSS, Telefon, adreca);
-//                control.hospitalPrueba.afegirPacient(pacient);
-                Creacion.escriuPacient(pacient);
+                PreparedStatement ps = null;
+                GestorConexion objCon = new GestorConexion();
+                Connection conn = objCon.getConexion();
+                try {
+
+                    String sql = "INSERT INTO PacientHistorial(nif,telefon,nom,cognom1,cognom2,numSS,codiPostal,ciutat,carrer,numero,planta)VALUES"
+                            + "(\"" + DNI + "\",\"" + Telefon + "\",\"" + nombre + "\",\"" + apellido1 + "\",\"" + apellido2 + "\",\"" 
+                            + NSS + "\",\"" + cp + "\",\"" + ciutat + "\",\"" + carrer + "\"," + numero + "," + planta + ");";
+                    System.out.println(sql);
+                    ps = conn.prepareStatement(sql);
+                    ps.execute();
+                } catch (SQLException ex) {
+                    Logger.getLogger(MenuAnyadirMetgeControlador.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
                 mAP.dispose();
 
-            }else{
+                //Adreca adreca = new Adreca(ciutat, cp, carrer, Integer.parseInt(numero), planta, Integer.parseInt(porta));
+                //Pacient pacient = new Pacient(nombre, apellido1, apellido2, DNI, NSS, Telefon, adreca);
+                //control.hospitalPrueba.afegirPacient(pacient);
+                //Creacion.escriuPacient(pacient);
+                //mAP.dispose();
+            } else {
                 mAP.lblConfirmar.setText(salida);
             }
         }
-            //BOTÓN CANCELAR
-            if (ae.getSource() == mAP.btnCancelar) {
-                mAP.dispose();
-            }
+        //BOTÓN CANCELAR
+        if (ae.getSource() == mAP.btnCancelar) {
+            mAP.dispose();
         }
-
     }
 
+}
